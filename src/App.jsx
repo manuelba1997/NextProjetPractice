1738689943e669458b906cb20d8fcc93d77988ad
manuelba1997 <manuelbrugues@gmail.com>
2025-01-31 20:20:59 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/*import { useState } from 'react'*/
+/*import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'*/
+import './App.css'
+import Greeting from './components/Greeting'
+
+function App() {
+  /*const [count, setCount] = useState(0)*/
+
+  return (
+    <>
+      <h1>Hello React</h1>
+      <p>Wellcome to my first React APP</p>
+      <Greeting />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,4 @@
+function Greeting(){
+    return <h2>Have a Great Day</h2>;
+}
+export default Greeting;
